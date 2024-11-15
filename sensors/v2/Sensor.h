LineageOS/android_hardware_xiaomi

sensors: Implement single tap sensor
Change-Id: I838121ffc627fb8091ce16e0ec4499f5f44bc446

sensors/Sensor.h
@@ -138,6 +138,17 @@ class UdfpsSensor : public SysfsPollingOneShotSensor {
     int mScreenY;
 };
 
+class SingleTapSensor : public SysfsPollingOneShotSensor {
+  public:
+    SingleTapSensor(int32_t sensorHandle, ISensorsEventCallback* callback)
+        : SysfsPollingOneShotSensor(
+                  sensorHandle, callback, "/sys/class/touch/touch_dev/gesture_single_tap_state",
+                  "/sys/class/touch/touch_dev/gesture_single_tap_enabled", "Single Tap Sensor",
+                  "org.lineageos.sensor.single_tap",
+                  static_cast<SensorType>(static_cast<int32_t>(SensorType::DEVICE_PRIVATE_BASE) +
+                                          2)) {}
+};
+
 }  // namespace implementation
 }  // namespace subhal
 }  // namespace V2_1

AOSP-for-venus/android_vendor_hardware_xiaomi

sensors/v2/Sensor.h
@@ -132,6 +132,17 @@ class DoubleTapSensor : public SysfsPollingOneShotSensor {
                                           1)) {}
 };
 
+class SingleTapSensor : public SysfsPollingOneShotSensor {
+  public:
+    SingleTapSensor(int32_t sensorHandle, ISensorsEventCallback* callback)
+        : SysfsPollingOneShotSensor(
+                  sensorHandle, callback, "/sys/class/touch/touch_dev/gesture_single_tap_state",
+                  "/sys/class/touch/touch_dev/gesture_single_tap_enabled", "Single Tap Sensor",
+                  "com.statixos.sensor.single_tap",
+                  static_cast<SensorType>(static_cast<int32_t>(SensorType::DEVICE_PRIVATE_BASE) +
+                                          2)) {}
+};
+
 }  // namespace implementation
 }  // namespace subhal
 }  // namespace V2_1